ed06c614c2222c9b6f2cea2971656962ec165979
claudiojpg <claudiovalencia13@gmail.com>
2023-10-15 20:28:57 -0300

    nuevas funcionalidades

--- Proyectos/examen/fundap/static/js/main.js
+++ Proyectos/examen/fundap/static/js/main.js
@@ -7,19 +7,38 @@ eye.addEventListener("click", function(){
   })
 
   function login(){
-    var user, password
+    var user, password;
     
     user = document.getElementById("usuario").value;
-    password= document.getElementById("password").value;
-
-
-    if(user == "admin" && password =="admin"){
-       location.href="datos_fundap"
-
-       
-    }
+    password = document.getElementById("password").value;
+  
     
-    else{
-     alert("error")    
+    if(user.trim() === "" && password.trim() === ""){
+      alert("Debes completar los campos");
+    } 
+    else if(user == "admin" && password == "admin"){
+      location.href="datos_fundap"; 
+    } 
+    else if(user != "admin") { 
+      
+      alert("El usuario no está registrado");
+    } 
+    else {
+      
+      alert("Contraseña incorrecta");
     }
-}
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+   
+    var submitButton = document.getElementById('submit-btn');
+  
+  
+    submitButton.addEventListener('click', function(event) {
+      
+      event.preventDefault();
+
+      
+      alert('Aporte realizado');
+    });
+  });
